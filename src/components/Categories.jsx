ba2687e940568c3411c6ce49be1210aeb4bdfe89
fredrikkri <fjkri@hotmail.com>
2024-02-15 09:32:56 +0100

    laget linker til de forskjellige kategoriene, de fungerer men det er ikke noe på de sidene

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,15 @@
+import {Link} from "react-router-dom"
+
 export default function Categories() {
+    const categories = ["Technology", "Food", "Fashion", "Health", "Sports", "Entertainment", "Home & Garden", "Automotive", "Travel", "Books"];
+    
     return (
-        <h1>Categories</h1>
+
+        <>
+            <h1>Categories</h1>
+            <ul>
+                {categories.map((category, index) => <li key={category + index}><Link to={category}>{category}</Link></li>)}
+            </ul>
+        </>
     )
 }
