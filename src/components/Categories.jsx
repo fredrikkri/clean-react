7685e42fa114477ed42d3da25e8251783796077a
fredrikkri <fjkri@hotmail.com>
2024-02-15 09:43:30 +0100

    e

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,15 @@
-import {Link} from "react-router-dom"
+import {Link, Outlet} from "react-router-dom"
 
 export default function Categories() {
     const categories = ["Technology", "Food", "Fashion", "Health", "Sports", "Entertainment", "Home & Garden", "Automotive", "Travel", "Books"];
     
     return (
-
         <>
             <h1>Categories</h1>
             <ul>
                 {categories.map((category, index) => <li key={category + index}><Link to={category}>{category}</Link></li>)}
             </ul>
+            <Outlet />
         </>
     )
 }
---
+++ src/components/CategoriesIndex.jsx
@@ -0,0 +1,5 @@
+export default function CategoriesIndex() {
+    return (
+        <h1>Index</h1>
+    )
+}
