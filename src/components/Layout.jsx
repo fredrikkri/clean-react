be6686a7d89667eeaa01f7985497e069f2dda007
fredrikkri <fjkri@hotmail.com>
2024-02-15 08:47:17 +0100

    laget default layout, dette skal være likt på alle sider

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+export default function Layout() {
+    return(
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>Kommer en link</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
